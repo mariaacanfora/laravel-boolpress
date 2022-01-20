5f5f2ea733ecc975a2308ac2823717dc13168b2a
mariaacanfora <ma.maria.acanfora@gmail.com>
2022-01-20 15:16:09 +0100

    commit requested by project manager

--- resources/js/components/partials/Post.vue
+++ resources/js/components/partials/Post.vue
@@ -3,7 +3,7 @@
         <div class="card-body">
             <h5 class="card-title">{{post.title}}</h5>
             <p class="card-text">{{post.text}}</p>
-            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            <!-- <a href="'/posts/' + post.id" class="btn btn-primary">Go somewhere</a> -->
         </div>
     </div>
 </template>
